3ca5ed878603154c0e19de72d13b815c2b2bb40d
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2022-11-03 19:47:55 +0400

    Add files via upload

---
+++ Лабораторная работа 5/Лабораторная работа 5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    //0
+    int arr[4][2] = { {-1,5},{1,-2},{-5,7},{4, -3} }; //Массив
+    int NCh = 0; //Нечётные элементы
+    int OtrNCh = 0; //Отрицательные нечётные элементы
+    int sum = 0; //Сумма модулей OtrNCh
+    cout << "ARRAY(MASSIV): " << endl << endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << " " << arr[i][j];
+        }
+        cout << " " << endl;
+    }
+    cout << endl;
+    //Найти нечётные элементы
+    //Найти отрицательные нечётные элементы
+    //Найти модуль отрицательных нечётных элементов
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (arr[i][j] % 2 != 0 && arr[i][j] % 2 >= 0) {
+                OtrNCh += 1;
+                sum = sum + fabs(OtrNCh);
+            }
+        }
+    }
+    cout << "SUM: " << sum << endl;
+    //1
+    
+
+    //2
+}
