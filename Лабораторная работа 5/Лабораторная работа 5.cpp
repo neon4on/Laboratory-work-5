3cd4fc4e20783a5f1bcfef3adda2a421459ae837
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2022-11-18 17:20:55 +0400

    Add files via upload

--- Лабораторная работа 5/Лабораторная работа 5.cpp
+++ Лабораторная работа 5/Лабораторная работа 5.cpp
@@ -1,36 +1,45 @@
 ﻿#include <iostream>
-#include <ctime>
+#include <cmath>
+#include <cstdlib>
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
     //0
-    int arr[4][2] = { {-1,5},{1,-2},{-5,7},{4, -3} }; //Массив
-    int NCh = 0; //Нечётные элементы
-    int OtrNCh = 0; //Отрицательные нечётные элементы
-    int sum = 0; //Сумма модулей OtrNCh
-    cout << "ARRAY(MASSIV): " << endl << endl;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 2; j++) {
-            cout << " " << arr[i][j];
-        }
-        cout << " " << endl;
+    int i, j;
+    const int n = 5, m = 5;
+    int NCh = 0; //Nechetnie elementi
+    int OtrNCh = 0; //Otricatelnie nechetnie elementi
+    long sum = 0; //summa moduley OTRNCH
+    int arr[n][m];
+    int** a = new int* [n];
+    for (i = 0; i < n; i++) {
+        a[i] = new int[m];
     }
-    cout << endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = rand() % 21 - 10;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << " " << arr [i][j]; //Vivod Massiva
+        }
+        cout << endl;
+    }
     //Найти нечётные элементы
     //Найти отрицательные нечётные элементы
     //Найти модуль отрицательных нечётных элементов
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 2; j++) {
-            if (arr[i][j] % 2 != 0 && arr[i][j] % 2 >= 0) {
+            if (arr [i][j] % 2 != 0 && arr[i][j] % 2 >= 0){
                 OtrNCh += 1;
                 sum = sum + fabs(OtrNCh);
             }
         }
     }
-    cout << "SUM: " << sum << endl;
+    cout << sum;
     //1
-    
-
     //2
 }
+
